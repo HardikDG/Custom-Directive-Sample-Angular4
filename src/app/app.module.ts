b713b496a7ce5e384e25d6064a63bfee05cc73ce
HardikDG <hardikdg.sa@gmail.com>
2017-05-14 17:36:21 +0530

    Update code with structural directives and ngSwitch implementation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,16 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
-import { BasicHighlightDirective } from './highlight/basic.highlight.directive';
-import { BetterHighlightDirective } from './highlight/better.highlight.directive';
+import { BasicHighlightDirective } from './directives/basic.highlight.directive';
+import { BetterHighlightDirective } from './directives/better.highlight.directive';
+import { UnlessDirective } from './directives/unless.directive';
 
 @NgModule({
   declarations: [
     AppComponent,
     BasicHighlightDirective,
-    BetterHighlightDirective
+    BetterHighlightDirective,
+    UnlessDirective
   ],
   imports: [
     BrowserModule,
--- src/app/directives/better.highlight.directive.ts
+++ src/app/directives/better.highlight.directive.ts
@@ -1,25 +1,28 @@
-import { Directive, Renderer2, OnInit, ElementRef,HostListener,HostBinding } from '@angular/core';
+import { Directive, Renderer2, OnInit, ElementRef,HostListener,HostBinding,Input } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]'
 })
 export class BetterHighlightDirective implements OnInit {
-  @HostBinding('style.backgroundColor') backgroundColor:string = 'transparent';
+  @Input() defaultColor:string = 'transparent';
+  @Input('appBetterHighlight') highlightColor:string = 'skyblue';
+  @HostBinding('style.backgroundColor') backgroundColor:string = this.defaultColor;
 
   constructor(private elementRef: ElementRef, private renderer: Renderer2) { }
 
   ngOnInit() {
+    this.backgroundColor = this.defaultColor;
     // this.renderer.setStyle(this.elementRef.nativeElement,'backgroundColor','blue');
   }
 
   @HostListener('mouseenter') mouseenter(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement,'backgroundColor','blue');
-    this.backgroundColor = 'skyblue';
+    this.backgroundColor = this.highlightColor;
   }
 
   @HostListener('mouseleave') mouseleave(eventData: Event) {
     // this.renderer.setStyle(this.elementRef.nativeElement,'backgroundColor','transparent');
-    this.backgroundColor = 'transparent';
+    this.backgroundColor = this.defaultColor;
   }
   
 }
